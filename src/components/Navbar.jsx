d99c31cea2874524d0783fd29a6ca3e9767add36
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-11 19:48:13 +0500

    basic navbar and search component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
 
+const Navbar = () => (
+	<Stack
+		direction="row"
+		alignItems="center"
+		p={2}
+		sx={{
+			position: "sticky",
+			top: 0,
+			background: "#000",
+			justifyContent: "space-between",
+		}}
+	>
+		<Link to="/" style={{ display: "flex", alignItems:'center' }}>
+			<img src={logo} alt = "logo" height={45} />
+		</Link>
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    <SearchBar />
+	</Stack>
+);
 
-export default Navbar
+export default Navbar;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
+const SearchBar = () => (
+    <Paper
+    component="form"
+    onSubmit={e => console.log(e.target.value)}
+    sx={{
+        borderRadius:20,
+        border: '1px solid #e3e3e3',
+        pl:2,
+        boxShadow:'none',
+        mr:{sm:5}
+    }}
+    >
+        <input
+            className="search-bar"
+            placeholder="Search..."
+      
+            onChange={()=>{}}
+        />
+        <IconButton type = "submit" sx={{p:'10px', color:'red'}}>
+            <Search />
+        </IconButton>
+    </Paper>
+)
+
+export default SearchBar;
