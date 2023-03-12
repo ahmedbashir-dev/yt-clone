7393530b3b6bf9b0304c117eba2fab77213a021d
Ahmed Bashir <ahmedbashir.w@gmail.com>
2023-03-12 12:35:06 +0500

    Feed implementation

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,35 @@
+import { Card, CardMedia, Typography, Box, CardContent } from "@mui/material"
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
+const ChannelCard = ({channelDetail}) => {
+  return (
+    <Box
+      sx={{
+        boxShadow:'none',
+        borderRadius:'20px'
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{display:'flex', flexDirection:'column' 
+          , justifyContent:'center', textAlign:'center', color:'#fff'
+        }}
+        >
+
+          <CardMedia
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+            alt = {channelDetail?.snippet?.title}
+            sx={{borderRadius:'50%', height:'180px', width:'180px', mb:2, border:'1px solid #e3e3e3'}}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: "14px", color: "gray", ml: "5px" }} />
+          </Typography>
+        </CardContent>
+      </Link>
+    </Box>
+  )
+}
+
+export default ChannelCard
